acb03fce655e6cd7606264faf4fad5a59b69b7ce
Stefan Mahr <stefan.mahr@sphairon.com>
2013-05-21 02:12:43 +0200

    rewrite redisSet with new helper functions and optimze matrix upload

--- redisSet.m
+++ redisSet.m
@@ -7,29 +7,23 @@ else
   if 1 == iscell(value)
 	disp('ERROR: cells are not supported!');
   elseif 1 < size(value,1) || 1 < size(value,2)
-	dim=sprintf('%d' ,size(value,2)); % just the colum nr is needed for restoring the original dimension of the matrice
-	ld=length(dim);
-	tcp_write(R,sprintf("*3\r\n$5\r\nRPUSH\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",lk,key,ld,dim)); % the first value in our lrange is not a value from the matrice, it's just the colume indice number for restoring the matrice dimension
-	values=reshape (value,[],1);
+	dim=sprintf('%d ' ,size(value)); % save original dimensions
+	__redisWrite(R, 'DEL', key);           % do not append, create new
+	 status=__redisRead(R, 5000);
+	    
+	__redisWrite(R, 'RPUSH', key, dim);
+	status=[status __redisRead(R, 5000)];
+	    
 	% if it's a huge matrice, it may take a long long time! it's not a fault of redis!!
-	for row = 1:size(values,1)
-		val=num2str(values(row),16);
-		lv=length(val);
-		tcp_write(R,sprintf("*3\r\n$5\r\nRPUSH\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",lk,key,lv,val));
-	end
-%% tried to vectorized it - but it fails atm
-%	values=num2str(values,7);
-%	vallen(1:size(values,1),1)=length(values(:,1:end));
-%	tcp_write(R,sprintf("*3\r\n$5\r\nRPUSH\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",lk,key,vallen(1:end),values(:,1:end)));
-%	status=char(tcp_read(R,10000000,100)); % set time out higher for multiple value feedback!
+	__redisWrite(R, 'RPUSH', key, value(:));
+    status=[status __redisRead(R, 5000)];
+    
   elseif 1 == isnumeric (value)
-	lv=length(num2str(value,16));
-	tcp_write(R,sprintf("*3\r\n$3\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",lk,key,lv,num2str(value,16)));
-	status=char(tcp_read(R,10000000,5));
+    __redisWrite(R, 'SET', key, value);
+	status=__redisRead(R, 5000);
   else
 	disp('WARNING: You are going to store a string! Take care when do redisGet in Octave! String import is not supported yet!');
-	lv=length(value);
-	tcp_write(R,sprintf("*3\r\n$3\r\nSET\r\n$%d\r\n%s\r\n$%d\r\n%s\r\n",lk,key,lv,value));
-	status=char(tcp_read(R,10000000,5));
+	__redisWrite(R, 'SET',key,value);
+	status=__redisRead(R, 5000);
   end
 end
