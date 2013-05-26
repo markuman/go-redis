b7dac5661f9c287430674c1a0d2e4423713eaf8f
Markus Bergholz <markuman@gmail.com>
2013-05-26 21:53:38 +0200

    fix redisCommand.

--- redisCommand.m
+++ redisCommand.m
@@ -1,9 +1,5 @@
-function value = redisCommant(R, cmd, arg, opt)
+function value = redisCommand(R, Command, opt)
 
-lc=length(cmd);
-la=length(arg);
-lo=length(opt);
-tcp_write(R,sprintf("*2\r\n$3\r\nGET\r\n$%d\r\n%s\r\n",lk,key));
-
-value=char(tcp_read(R,10000000,1000));
+__redisWrite(R, Command, opt);
+value = __redisRead(R, 5000);
 
