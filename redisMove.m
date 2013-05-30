f7a9885143794061fedc59a7fc84e06ba9f87cb7
Markus Bergholz <markuman@gmail.com>
2013-05-30 12:27:25 +0200

    improve error messages

--- redisMove.m
+++ redisMove.m
@@ -3,10 +3,6 @@ function value = redisMove(R, key, db)
 __redisWrite(R, 'MOVE', key, db);
 value = __redisRead(R, 5000);
 
-if strcmp(value,":1\r\n")
-  sprintf('Key %s successful moved to database %d!', key, db)
-elseif strcmp(value,":0\r\n")
+if !strcmp(value,":1\r\n")
   sprintf('WARNING: Failed to move key %s to database %d!', key, db)
-else
-  sprintf('ERROR!')
 end
