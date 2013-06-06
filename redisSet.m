430c6499b28c5a6d75f9bab91942e947cb4eac56
Markus Bergholz <markuman@gmail.com>
2013-06-06 22:54:58 +0200

    fix for multidimensional matrices

--- redisSet.m
+++ redisSet.m
@@ -6,7 +6,13 @@ else
   lk=length(key);
   if 1 == iscell(value)
 	disp('ERROR: cells are not supported!');
-  elseif 1 < size(value,1) || 1 < size(value,2)
+  elseif 1 == ischar(value)
+        __redisWrite(R, 'SET',key,value);
+        status=__redisRead(R, 5000);
+  elseif 1 == size(value,1) && 1 == size(value,2) && 1 == isnumeric (value)
+	__redisWrite(R, 'SET', key, value);
+        status=__redisRead(R, 5000);
+  elseif 2 <= ndims(value)
 	dim=sprintf('%d ' ,size(value)); % save original dimensions
         % do not append, DEL and recreate!
         __redisWrite(R, 'EXISTS', key);
@@ -20,14 +26,8 @@ else
 	status=[status __redisRead(R, 5000)];
 
 	__redisWrite(R, 'RPUSH', key, value(:));
-    status=[status __redisRead(R, 5000)];
-
-  elseif 1 == isnumeric (value)
-    __redisWrite(R, 'SET', key, value);
-	status=__redisRead(R, 5000);
+	status=[status __redisRead(R, 5000)];
   else
-	disp('WARNING: You are going to store a string! Take care when do redisGet in Octave! String import is not supported yet!');
-	__redisWrite(R, 'SET',key,value);
-	status=__redisRead(R, 5000);
+	disp('ERROR: I do not understand what you are traing so save!')
   end
 end
